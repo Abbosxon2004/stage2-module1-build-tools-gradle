badffbf990af7c24d04aa664681ea9e8856e63e6
abboskhan <abbosxon28012004@gmail.com>
2023-02-06 14:08:07 +0500

    some_changing

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        try {
+            return Double.parseDouble(str) > 0;
+        } catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
          //here magic will happen
     }
 }
